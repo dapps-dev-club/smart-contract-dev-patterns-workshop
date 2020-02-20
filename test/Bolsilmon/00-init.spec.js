33dfdb7797c05d77308992ea3ceca00234076054
bguiz <bguiz@users.noreply.github.com>
2020-02-20 21:17:21 +0800

    test: for main contract initialisation (5)

--- test/Bolsilmon/00-init.spec.js
+++ test/Bolsilmon/00-init.spec.js
@@ -3,4 +3,6 @@ const assert = require('assert');
 const Bolsilmon = artifacts.require('Bolsilmon');
 
 contract('Bolsilmon - initial state', () => {
+  it('should initialise', async () => {
+  });
 });
