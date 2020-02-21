b62df228a6ffb5531ce31728640f9b0c93b7bccb
bguiz <bguiz@users.noreply.github.com>
2020-02-21 16:38:22 +0800

    test: for createMon function (3)

--- test/Bolsilmon/01-create-mon.spec.js
+++ test/Bolsilmon/01-create-mon.spec.js
@@ -1,5 +1,11 @@
 const assert = require('assert');
 
+const {
+  BN,
+  expectRevert,
+  expectEvent,
+} = require('@openzeppelin/test-helpers');
+
 const Bolsilmon = artifacts.require('Bolsilmon');
 
 contract('Bolsilmon - createMon', () => {
