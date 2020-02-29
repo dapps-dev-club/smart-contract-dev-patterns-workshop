3813e8f0fba810413f0b67eff107934c53191e82
bguiz <bguiz@users.noreply.github.com>
2020-02-29 17:45:44 +0800

    test: for birthMon function (2)

---
+++ test/Bolsilmon/02-birth-mon.spec.js
@@ -0,0 +1,10 @@
+
+const assert = require('assert');
+
+const {
+  BN,
+  expectEvent,
+  expectRevert,
+} = require('@openzeppelin/test-helpers');
+
+const Bolsilmon = artifacts.require('Bolsilmon');
