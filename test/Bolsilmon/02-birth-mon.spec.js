7baa86f2b0608712c3e87c60787ce3ec6dabc01e
bguiz <bguiz@users.noreply.github.com>
2020-02-29 17:46:02 +0800

    test: for birthMon function (3)

--- test/Bolsilmon/02-birth-mon.spec.js
+++ test/Bolsilmon/02-birth-mon.spec.js
@@ -8,3 +8,14 @@ const {
 } = require('@openzeppelin/test-helpers');
 
 const Bolsilmon = artifacts.require('Bolsilmon');
+
+contract('Bolsilmon - birthMon', (accounts) => {
+  const [
+    account1,
+    account2,
+  ] = accounts;
+
+  const geneSeed =
+    '0x0102030405060708090a0b0c0d0e0f100102030405060708090a0b0c0d0e0f10';
+
+});
